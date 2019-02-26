7d41c66ce761a7273aea182f799b803dcafbe56b
wangrex <503431920@qq.com>
2019-02-26 20:51:52 +0800

    Update Requirement details title.

---
+++ AutoGeneratorMVC5/Apps.DAL/App/App_ApplyJobStepRepository.cs
@@ -0,0 +1,27 @@
+﻿using Apps.Models;
+using Apps.Models.App;
+using Apps.IDAL.App;
+using System;
+namespace Apps.DAL.App
+{
+    public partial class App_ApplyJobStepRepository
+    {
+        #region 获取步骤对应名称
+        /// <summary>
+        /// 获取步骤对应名称
+        /// </summary>
+        /// <param name="Step"></param>
+        /// <returns></returns>
+        public string GetStepName(string Step)
+        {
+            string StepName = "";
+            var ApplyJobStep = Find(EF => EF.Step == Step);
+            if (null != ApplyJobStep)
+            {
+                StepName = ApplyJobStep.Name;
+            }
+            return StepName;
+        }
+        #endregion
+    }
+}
