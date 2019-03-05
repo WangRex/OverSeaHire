6e9b44d3daa0883e0392bd4e7e3e201e1cc0ed11
wangrex <503431920@qq.com>
2019-03-05 22:31:21 +0800

    添加清空数据Sql

---
+++ Documents/SQL/清空数据.sql
@@ -0,0 +1,17 @@
+USE [AppsDBOverseasHire]
+GO
+
+DELETE FROM [dbo].App_Customer;
+
+DELETE FROM [dbo].App_ApplyJobRecord;
+
+DELETE FROM [dbo].App_ApplyJob;
+
+DELETE FROM [dbo].App_Requirement;
+
+DELETE FROM [dbo].SysLog;
+
+DELETE FROM [dbo].SysMessage;
+GO
+
+
